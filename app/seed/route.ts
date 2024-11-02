3ca8b2dc0f68b5a2e4908dfb7e59cc1208808e45
Stephanie Arjune <steffyarjune@gmail.com>
2024-11-01 20:53:11 -0400

    updated route.ts
    removed ... return response.json

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,10 +102,6 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
  }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
    try {
      await client.sql`BEGIN`;
      await seedUsers();
